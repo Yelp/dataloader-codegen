4b404e5c069de083874e1a61a6925d12e5fff645
Jiahui Ruan <jruan@yelp.com>
2020-03-18 16:42:57 -0700

    add tests for unPartitionResults

---
+++ __tests__/runtimeHelpers.test.js
@@ -0,0 +1,29 @@
+import { unPartitionResults } from '../src/runtimeHelpers';
+
+describe('unPartitionResults', () => {
+    it('should perform inverse mapping for result without Error', () => {
+        expect(unPartitionResults([[0, 2], [1]], [[{ foo: 'foo' }, { bar: 'bar' }], [{ baz: 'baz' }]])).toEqual([
+            { foo: 'foo' },
+            { baz: 'baz' },
+            { bar: 'bar' },
+        ]);
+    });
+
+    it('should perform inverse mapping for result with Error in resultGroups', () => {
+        const customError = new Error('bar error');
+        expect(unPartitionResults([[0, 2], [1]], [[{ foo: 'foo' }, customError], [{ baz: 'baz' }]])).toEqual([
+            { foo: 'foo' },
+            { baz: 'baz' },
+            customError,
+        ]);
+    });
+
+    it('should perform inverse mapping for result with Error as an item in resultGroups', () => {
+        const customError = new Error('foo error');
+        expect(unPartitionResults([[0, 2], [1]], [customError, [{ baz: 'baz' }]])).toEqual([
+            customError,
+            { baz: 'baz' },
+            customError,
+        ]);
+    });
+});
